6ad9f577522372e83f81294ee67c751790d0e25c
robertjurczak1 <robertjurczak@wp.pl>
2023-10-09 13:02:46 +0200

    9.10 12.31 zmieniłem treść w plikach

---
+++ tekst.py
@@ -0,0 +1,9 @@
+wiek = 2023 - 1978
+
+str1 = 'to jest tekst 1\n'
+str2 = f"wiek = {wiek}\n"
+str3 = f"""to jest też wiek {wiek},
+          ale ma specjalne opcje
+          i tu kolejny tekst"""
+
+print(str1, str2, str3)
