70f05b43760299e29a7b80754f1b109a0aaaaf74
robertjurczak1 <robertjurczak@wp.pl>
2023-10-09 12:33:00 +0200

    9.10 12.31 zmieniłem treść w plikach

---
+++ liczby.py
@@ -0,0 +1,20 @@
+import sys
+
+a = 15
+b = - 6
+print("wynik dodawania:", a + b)
+print("wynik odejmowania:", a - b)
+print("wynik mnożenia:", a * b)
+print("wynik dzielenia:", a / b)
+print("wynik:", a // b)
+print("wynik:", a ** b)
+print("wynik:", a % b)
+print("wynik:", a + True)
+print("wynik:", a + False)
+
+pi = 3.14
+radius = 4.5666
+area = pi * (radius ** 2)
+print(area)
+
+print(sys.float_info)
---
+++ start.py
@@ -0,0 +1,10 @@
+print("Cześć Python!")
+rok = 2023
+print(type(rok))
+imie = "Robert"
+print(type(imie))
+temp = 36.9
+print(type(temp))
+czy_jest_zimno = True
+print(type(czy_jest_zimno))
+# komentarz
